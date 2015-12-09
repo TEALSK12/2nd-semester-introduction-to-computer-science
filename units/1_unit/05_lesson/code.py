754e0e69144b0ade361285db80813c468ff8c635
thatgoldblatt <suzgoldblatt@gmail.com>
2015-12-09 12:56:45 -0500

    Delete units/1_unit/05_lesson/code.py

--- units/1_unit/05_lesson/code.py
+++
@@ -1,5 +0,0 @@
-favorite_number_str = input("What is your favorite number: ")
-birth_month_str = input("What month where you born in: ")
-
-lucky_number = int(favorite_number_str) + int(birth_month_str)
-print("Your lucky number is " + lucky_number)
