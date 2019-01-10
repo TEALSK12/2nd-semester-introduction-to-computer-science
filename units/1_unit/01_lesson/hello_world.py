a89a07e1b6795568e7710e40d5441227586d62f5
Dan-TEALS <dan@tealsk12.org>
2019-01-09 18:17:18 -0700

    Fix comment and indentation
    Fixed header comments to get rid of IntroCS A (which made no sense) and changed indentation to 4 spaces for consistency with PEP 8.

--- units/1_unit/01_lesson/hello_world.py
+++ units/1_unit/01_lesson/hello_world.py
@@ -1,10 +1,10 @@
 # last_name, first_name
-# 11/30/2015
-# IntroCS A
+# 11/30/2019
+# Intro to CS
 
 print("hello world")
 
 def my_function(name):
-	print("hello world, " + name)
+    print("hello world, " + name)
 
 my_function("Susan")
