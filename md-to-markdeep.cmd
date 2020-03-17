614e8def6443a2c581beb4a4ad93c742bd3cfa5c
Andrew Spiece <andrew@tealsk12.org>
2020-03-17 19:31:09 -0400

    Update

--- md-to-markdeep.cmd
+++ md-to-markdeep.cmd
@@ -1,7 +1,7 @@
 @REM echo off & setlocal
 
 @REM -- Set up the GitHub pages directory structure by copying curriculum content.
-robocopy /mir .\units .\docs
+robocopy /mir .\ ..\docs
 
 @REM -- Move to docs directory
 cd .\docs
@@ -15,20 +15,20 @@ for %%f in (*.md) do (
     type >>%%f.html ..\markdeep-footer.txt
 
     @REM -- Create .pdf version of markdown files
-    pandoc --pdf-engine=xelatex -V geometry:margin=2cm "%%f" -o "%%~nf.pdf"    
+    @REM pandoc --pdf-engine=xelatex -V geometry:margin=2cm "%%f" -o "%%~nf.pdf"    
+    
 
-)
 
 @REM -- Convert files in each unit
 
-for /r . %%f in (*.md) do (
+for /r .. %%f in (*.md) do (
     @echo %%f
     type >%%f.html ..\markdeep-header.txt
     %sed% >>%%f.html "s/\.md/.md.html/g" %%f
     type >>%%f.html ..\markdeep-footer.txt
 
     @REM -- Create .pdf version of markdown files
-    pandoc --pdf-engine=xelatex -V geometry:margin=2cm "%%f" -o "%%~nf.pdf" 
+    @REM pandoc --pdf-engine=xelatex -V geometry:margin=2cm "%%f" -o "%%~nf.pdf" 
 )
 
 copy SUMMARY.md.html index.html
