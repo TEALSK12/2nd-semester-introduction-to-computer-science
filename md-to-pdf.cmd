b8a3fdb4028bc3f34aa71100d39dd1172f10ba37
Andrew Spiece <andrew@tealsk12.org>
2020-07-20 17:28:10 -0400

    mdtopdf

--- md-to-pdf.cmd
+++ md-to-pdf.cmd
@@ -10,7 +10,7 @@ echo %time%
 @REM usage to capture output and errors: md-tomarkdeep > out.txt 2>&1
 
 @REM -- Set up the GitHub pages directory structure by copying curriculum content.
-robocopy /E .\curriculum .\docs
+robocopy /E .\ .\docs
 
 @REM -- Move to docs directory
 cd .\docs
@@ -37,25 +37,34 @@ for /r . %%f in (*.md) do (
 
 )
 
-pdftk README.pdf Video-Tutorials.pdf Assets.pdf  Unit1\Unit1-Map.pdf Unit2\Unit2-Map.pdf Unit3\Unit3-Map.pdf Unit4\Unit4-Map.pdf Unit5\Unit5-Map.pdf Unit6\Unit6-Map.pdf Unit7\Unit7-Map.pdf Unit8\Unit8-Map.pdf Unit9\Unit9-Map.pdf UnitA\UnitA-Map.pdf ^
-Unit1\Lesson-101.pdf Unit1\Lesson-102.pdf Unit1\Lesson-103.pdf Unit1\Lesson-104.pdf Unit1\Lesson-105.pdf Unit1\Lesson-106.pdf Unit1\Lesson-107.pdf Unit1\Lesson-108.pdf Unit1\Lesson-109.pdf Unit1\Lesson-1XX.pdf ^
-Unit2\Lesson-200.pdf Unit2\Lesson-201.pdf Unit2\Lesson-202.pdf Unit2\Lesson-203.pdf Unit2\Lesson-204.pdf Unit2\Lesson-205.pdf Unit2\Lesson-206.pdf Unit2\Lesson-207.pdf Unit2\Lesson-208.pdf Unit2\Lesson-208a.pdf Unit2\Lesson-209.pdf Unit2\Lesson-210.pdf Unit2\Lesson-211.pdf ^
-Unit3\Lesson-300.pdf Unit3\Lesson-301.pdf Unit3\Lesson-302.pdf Unit3\Lesson-303.pdf Unit3\Lesson-304.pdf Unit3\Lesson-305.pdf Unit3\Lesson-306.pdf Unit3\Lesson-307.pdf Unit3\Lesson-308.pdf Unit3\Lesson-309.pdf Unit3\Lesson-310.pdf Unit3\Lesson-311.pdf Unit3\Lesson-312.pdf Unit3\Lesson-313.pdf Unit3\Lesson-314.pdf Unit3\Lesson-315.pdf Unit3\Lesson-316.pdf Unit3\Lesson-317.pdf Unit3\Lesson-318.pdf Unit3\Lesson-319.pdf  Unit3\Lesson-3XX1.pdf ^
-Unit4\Lesson-400.pdf Unit4\Lesson-401.pdf Unit4\Lesson-402.pdf Unit4\Lesson-403.pdf Unit4\Lesson-404.pdf Unit4\Lesson-405.pdf Unit4\Lesson-406.pdf Unit4\Lesson-407.pdf Unit4\Lesson-408.pdf Unit4\Lesson-409.pdf Unit4\Lesson-409a.pdf Unit4\Lesson-410.pdf Unit4\Lesson-4XX.pdf ^
-Unit5\Lesson-500.pdf Unit5\Lesson-501.pdf Unit5\Lesson-502.pdf Unit5\Lesson-503.pdf Unit5\Lesson-504.pdf Unit5\Lesson-505.pdf Unit5\Lesson-506.pdf Unit5\Lesson-506a.pdf Unit5\Lesson-507.pdf Unit6\Lesson-600.pdf Unit5\Lesson-5XX.pdf ^
-Unit6\Lesson-601.pdf Unit6\Lesson-602.pdf Unit6\Lesson-603.pdf Unit6\Lesson-604.pdf Unit6\Lesson-605.pdf Unit6\Lesson-606.pdf Unit6\Lesson-607.pdf Unit6\Lesson-607a.pdf Unit6\Lesson-608.pdf Unit6\Lesson-609.pdf ^
-Unit7\Lesson-700.pdf Unit7\Lesson-701.pdf Unit7\Lesson-702.pdf Unit7\Lesson-703.pdf Unit7\Lesson-704.pdf ^
-Unit8\Lesson-800.pdf Unit8\Lesson-801.pdf Unit8\Lesson-802.pdf Unit8\Lesson-803.pdf Unit8\Lesson-804.pdf Unit8\Lesson-805.pdf Unit8\Lesson-806.pdf Unit8\Lesson-808.pdf ^
-Unit9\Lesson-900.pdf Unit9\Lesson-901.pdf ^
-finalproject\CharacterClashProject.pdf ^
-culturedays\culture_day_lesson_a.pdf culturedays\culture_day_lesson_b.pdf culturedays\culture_day_lesson_c.pdf culturedays\culture_day_lesson_d.pdf ^
-About.pdf CONTRIBUTING.pdf cat output AP-CSA.pdf
+pdftk README.pdf curriculum_map.pdf units/2_unit/unit1_md.pdf units/1_unit/01_lesson/lesson.pdf units/1_unit/02_lesson/lesson.pdf units/1_unit/02_lesson/lab.pdf units/1_unit/03_lesson/lesson.pdf units/1_unit/03_lesson/do_now.pdf ^
+units/1_unit/03_lesson/lab.pdf units/1_unit/04_lesson/lesson.pdf units/1_unit/04_lesson/do_now.pdf units/1_unit/04_lesson/lab.pdf units/1_unit/05_lesson/lesson.pdf units/1_unit/06_lesson/lesson.pdf ^
+units/1_unit/06_lesson/project.pdf units/1_unit/06_lesson/alternate_project.pdf units/2_unit/unit2_md.pdf units/2_unit/01_lesson/lesson.pdf units/2_unit/01_lesson/do_now.pdf units/2_unit/01_lesson/lab.pdf ^
+units/2_unit/02_lesson/lesson.pdf units/2_unit/02_lesson/do_now.pdf units/2_unit/02_lesson/lab.pdf units/2_unit/03_lesson/lesson.pdf units/2_unit/03_lesson/do_now.pdf units/2_unit/03_lesson/lab.pdf ^
+units/2_unit/04_lesson/lesson.pdf units/2_unit/04_lesson/do_now.pdf units/2_unit/04_lesson/lab.pdf units/2_unit/05_lesson/lesson.pdf units/2_unit/05_lesson/do_now.pdf units/2_unit/05_lesson/lab.pdf ^
+units/2_unit/06_lesson/lesson.pdf units/2_unit/06_lesson/do_now.pdf units/2_unit/06_lesson/lab.pdf units/2_unit/07_lesson/lesson.pdf units/2_unit/07_lesson/project.pdf units/2_unit/07_lesson/alternate_project.pdf ^
+units/3_unit/unit_3.pdf units/3_unit/01_lesson/lesson.pdf units/3_unit/01_lesson/do_now.pdf units/3_unit/01_lesson/lab.pdf units/3_unit/02_lesson/lesson.pdf units/3_unit/02_lesson/do_now.pdf ^
+units/3_unit/02_lesson/lab.pdf units/3_unit/03_lesson/lesson.pdf units/3_unit/03_lesson/do_now.pdf units/3_unit/03_lesson/lab.pdf units/3_unit/04_lesson/lesson.pdf units/3_unit/04_lesson/do_now.pdf ^
+units/3_unit/04_lesson/lab.pdf units/3_unit/05_lesson/lesson.pdf units/3_unit/05_lesson/project.pdf units/3_unit/05_lesson/alternate_project.pdf units/4_unit/unit4.pdf units/4_unit/01_lesson/lesson.pdf ^
+units/4_unit/01_lesson/do_now.pdf units/4_unit/01_lesson/lab.pdf units/4_unit/02_lesson/lesson.pdf units/4_unit/02_lesson/do_now.pdf units/4_unit/02_lesson/lab.pdf units/4_unit/03_lesson/lesson.pdf ^
+units/4_unit/03_lesson/do_now.pdf units/4_unit/03_lesson/lab.pdf units/4_unit/04_lesson/lesson.pdf units/4_unit/04_lesson/do_now.pdf units/4_unit/04_lesson/lab.pdf units/4_unit/05_lesson/lesson.pdf ^
+units/4_unit/05_lesson/lab.pdf units/4_unit/06_lesson/lesson.pdf units/4_unit/06_lesson/project.pdf units/4_unit/06_lesson/alternate_project.pdf units/5_unit/unit5.pdf units/5_unit/01_lesson/lesson.pdf ^
+units/5_unit/01_lesson/do_now.pdf units/5_unit/01_lesson/lab.pdf units/5_unit/02_lesson/lesson.pdf units/5_unit/02_lesson/do_now.pdf units/5_unit/02_lesson/lab.pdf units/5_unit/03_lesson/lesson.pdf ^
+units/5_unit/03_lesson/do_now.pdf units/5_unit/03_lesson/lab.pdf units/5_unit/04_lesson/lesson.pdf units/5_unit/04_lesson/do_now.pdf units/5_unit/04_lesson/lab.pdf units/5_unit/05_lesson/lesson.pdf ^
+units/5_unit/05_lesson/do_now.pdf units/5_unit/05_lesson/project.pdf units/6_unit/unit6.pdf units/6_unit/01_lesson/lesson.pdf units/6_unit/01_lesson/do_now.pdf units/6_unit/01_lesson/lab.pdf units/6_unit/02_lesson/lesson.pdf ^
+units/6_unit/02_lesson/do_now.pdf units/6_unit/02_lesson/lab.pdf units/6_unit/03_lesson/lesson.pdf units/6_unit/03_lesson/do_now.pdf units/6_unit/03_lesson/lab.pdf units/6_unit/04_lesson/lesson.pdf ^
+units/6_unit/04_lesson/do_now.pdf units/6_unit/04_lesson/lab.pdf units/6_unit/05_lesson/lesson.pdf units/6_unit/05_lesson/do_now.pdf units/6_unit/05_lesson/projectb.pdf units/6_unit/05_lesson/project.pdf ^
+units/7_unit/unit7.pdf units/7_unit/01_lesson/lesson.pdf units/7_unit/01_lesson/do_now.pdf units/7_unit/01_lesson/example.pdf units/7_unit/01_lesson/lab.pdf units/7_unit/02_lesson/lesson.pdf ^
+units/7_unit/02_lesson/do_now.pdf units/7_unit/02_lesson/lab.pdf units/7_unit/03_lesson/lesson.pdf units/7_unit/03_lesson/do_now.pdf units/7_unit/03_lesson/lab.pdf units/7_unit/04_lesson/lesson.pdf ^
+units/7_unit/04_lesson/do_now.pdf units/7_unit/04_lesson/lab.pdf units/7_unit/05_lesson/lesson.pdf units/7_unit/05_lesson/project.pdf units/7_unit/05_lesson/alternate_project.pdf units/8_unit/unit8.pdf ^
+units/8_unit/project.pdf units/8_unit/01_lesson/lesson.pdf units/8_unit/01_lesson/do_now.pdf units/8_unit/02_lesson/lesson.pdf units/8_unit/02_lesson/do_now.pdf units/8_unit/03_lesson/lesson.pdf ^
+units/8_unit/03_lesson/do_now.pdf units/8_unit/04_lesson/lesson.pdf units/8_unit/04_lesson/do_now.pdf units/supplemental/.pdf units/supplemental/01_lesson/lesson.pdf units/supplemental/01_lesson/do_now.pdf ^
+readings.pdf cat output 2ndSemPython.pdf
 
 @REM delete all temporary .pdf files  
-move /Y AP-CSA.pdf AP-CSA.save
+move /Y 2ndSemPython.pdf 2ndSemPython.save
 del /s *.pdf
-move /Y AP-CSA.save AP-CSA.pdf
-
+move /Y 2ndSemPython.save 2ndSemPython.pdf
 
 cd ..
 
