285fa5bd6348fc4dec1590cc596d71757339320d
aspiece <andrew@tealsk12.org>
2021-03-09 15:58:59 -0500

    HTML build and documents updates

--- docs/units/2_unit/04_lesson/Starter_food_chooser.py
+++ docs/units/2_unit/04_lesson/Starter_food_chooser.py
@@ -1,9 +1,10 @@
-food = ['donuts', 'pancakes', 'bacon', 'waffles','eggs','baggles']
+food = ['donuts', 'pancakes', 'bacon', 'waffles', 'eggs', 'bagels']
 score = [0,0,0,0,0,0]
 
-print('Please answer each questions with "y" for "yes" and "n" for "no."')
+print('Please answer each question with "y" for "yes" and "n" for "no".')
 user_input = input('Do you like food with holes? ')
 if user_input == 'y':
   score[0] = score[0] + 1
   score[5] = score[5] + 1
 
+# Add remaining questions here, and adjust scores similar to the above.
