9c5cfd4dabbbcb9789d4144da1de8f8d02fd3f58
ahmusic2009 <ahmusic2009@gmail.com>
2022-12-01 15:24:22 -0800

    update

---
+++ units/2_unit/04_lesson/Game_Show.py
@@ -0,0 +1,3 @@
+prizes = ['ball', 'hat', 'candy', 'cards', '$5', ' $20', '$100', 'Car', 'computer', 'ipad']
+num = (int(input('Pick and number between 1 and 10: ')) - 1)
+print('Your prize is: ' +prizes[num])
